dc24f52f306201620360fae8c14d54259577c5e5
AlexLiuDev233 <wzylin11@outlook.com>
2025-07-17 01:12:51 +0800

    fix: 持久模式通过切换物品栏来强制收杆
    我是真没想到YC这bug连收杆都做不到....

--- src/main/java/me/alexliudev/ycithelper/YcithelperClient.java
+++ src/main/java/me/alexliudev/ycithelper/YcithelperClient.java
@@ -204,7 +204,8 @@ public class YcithelperClient implements ClientModInitializer {
         if (scheduleOfPersistentFishing >= 20 * config.getPersistentFishingTimeout()) {
             scheduleOfPersistentFishing = 0;
             // 检测!
-            useItem(minecraftClient);
+            client.player.getInventory().selectedSlot = 0;
+            client.getNetworkHandler().sendPacket(new UpdateSelectedSlotC2SPacket(0));
             useItem(minecraftClient);
         }
     }
